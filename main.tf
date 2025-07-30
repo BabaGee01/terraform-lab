83d16e10abb283b822279feef61f796030050c2e
BabaGee01 <jide_oloko@hotmail.co.uk>
2025-07-30 17:56:52 +0100

    Terraform Updates

--- main.tf
+++ main.tf
@@ -45,7 +45,12 @@ resource "aws_subnet" "main" {
     cidr_block = "10.0.1.0/24"
     availability_zone = "eu-west-2a"
     map_public_ip_on_launch = true
+
+    tags = {
+    Name = "main-subnet"
+  }
 }
+
 
 resource "aws_route_table_association" "a" {
     subnet_id = aws_subnet.main.id
